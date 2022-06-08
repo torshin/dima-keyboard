00375573f424e42ae80b34276562942ac29a4c32
dtorshin <dtorshin@moysklad.ru>
2022-06-08 18:00:39 +0300

    switch lang on win+space

--- keyboards/dima2/keymaps/default/keymap.c
+++ keyboards/dima2/keymaps/default/keymap.c
@@ -12,7 +12,7 @@ enum layers {
 #define RAISE_T(kc) LT(_RAISE, kc)
 #define LOWER_T(kc) LT(_LOWER, kc)
 
-#define SWT_LNG LALT(KC_LSFT)
+#define SWT_LNG LGUI(KC_SPC)
 #define SFT_TAB LSFT(KC_TAB)
 
 // ,(?![^(\n]*\))
@@ -22,7 +22,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 RGUI_T(KC_GRV)     , KC_Q   , KC_W            , KC_E    , KC_R         , KC_T                                                                                          , KC_Y               , KC_U                , KC_I    , KC_O    , KC_P    , RGUI_T(KC_LBRC) ,
 LCTL_T(KC_ESC)     , KC_A   , KC_S            , KC_D    , KC_F         , KC_G                                                                                          , KC_H               , KC_J                , KC_K    , KC_L    , KC_SCLN , RCTL_T(KC_QUOT) ,
 KC_LALT            , KC_Z   , KC_X            , KC_C    , KC_V         , KC_B              , SWT_LNG                                                 , SWT_LNG         , KC_N               , KC_M                , KC_COMM , KC_DOT  , KC_SLSH , RALT_T(KC_RBRC) ,
-											    KC_LGUI , LT(_NUM, KC_RSFT) , LT(_RAISE,KC_TAB) , LSFT_T(KC_SPC) , LT(_LOWER, KC_DEL) , LT(_LOWER,KC_ESC) , RSFT_T(KC_BSPC) , LT(_RAISE, KC_ENT) , LT(_MOUSE, KC_RSFT) , KC_BSLS
+											    KC_LGUI , LT(_NUM, KC_RALT) , LT(_RAISE,KC_TAB) , LSFT_T(KC_SPC) , LT
+											    (_LOWER, KC_DEL) , LT(_LOWER,KC_ESC) , RSFT_T(KC_BSPC) , LT(_RAISE, KC_ENT) , LT(_MOUSE, KC_RSFT) , KC_BSLS
 )                  ,
 
 [_LOWER] = LAYOUT(
