076b944635da7ac58cbe7de729ec8f0adb4031ae
dtorshin <dtorshin@moysklad.ru>
2022-07-05 13:14:13 +0300

    edit grave key

--- keyboards/dima2/keymaps/default/keymap.c
+++ keyboards/dima2/keymaps/default/keymap.c
@@ -19,10 +19,10 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_MAIN] = LAYOUT(
-RGUI_T(KC_GRV)     , KC_Q   , KC_W            , KC_E    , KC_R        , KC_T                                                                                          , KC_Y               , KC_U                , KC_I    , KC_O    , KC_P    , RGUI_T(KC_LBRC) ,
-LCTL_T(KC_ESC)     , KC_A   , KC_S            , KC_D    , KC_F        , KC_G                                                                                          , KC_H               , KC_J                , KC_K    , KC_L    , KC_SCLN , RCTL_T(KC_QUOT) ,
-KC_LALT            , KC_Z   , KC_X            , KC_C    , KC_V        , KC_B              , LT(_NUM, SWT_LNG)                                       , SWT_LNG         , KC_N               , KC_M                , KC_COMM , KC_DOT  , KC_SLSH , RALT_T(KC_RBRC) ,
-											    KC_LGUI , LSFT(KC_TAB), LT(_RAISE,KC_TAB) , LSFT_T(KC_SPC) , LT(_LOWER, KC_DEL) , LT(_LOWER,KC_ESC) , RSFT_T(KC_BSPC) , LT(_RAISE, KC_ENT) , LT(_MOUSE, KC_RSFT) , KC_BSLS
+KC_GRV        , KC_Q   , KC_W , KC_E    , KC_R        , KC_T                                                                                          , KC_Y               , KC_U                , KC_I    , KC_O    , KC_P    , RGUI_T(KC_LBRC) ,
+LCTL_T(KC_ESC), KC_A   , KC_S , KC_D    , KC_F        , KC_G                                                                                          , KC_H               , KC_J                , KC_K    , KC_L    , KC_SCLN , RCTL_T(KC_QUOT) ,
+KC_LALT       , KC_Z   , KC_X , KC_C    , KC_V        , KC_B              , LT(_NUM, SWT_LNG)                                       , SWT_LNG         , KC_N               , KC_M                , KC_COMM , KC_DOT  , KC_SLSH , RALT_T(KC_RBRC) ,
+								KC_LGUI , LSFT(KC_TAB), LT(_RAISE,KC_TAB) , LSFT_T(KC_SPC) , LT(_LOWER, KC_DEL) , LT(_LOWER,KC_ESC) , RSFT_T(KC_BSPC) , LT(_RAISE, KC_ENT) , LT(_MOUSE, KC_RSFT) , KC_BSLS
 )                  ,
 
 [_LOWER] = LAYOUT(
@@ -36,7 +36,7 @@ LALT_T(KC_EQL)     , KC_1    , KC_2           , KC_3    , KC_4         , KC_5   
 _______            , KC_SLCK  , KC_PGUP       , (KC_UP)   , LCTL(KC_HOME) , KC_VOLU                                                                             , RALT(KC_T)      , KC_HOME         , KC_UP           , KC_PGUP , KC_SLCK , _______       ,
 LCTL_T(KC_APP)     , KC_INS   , LCTL(KC_LEFT) , (KC_DOWN) , LCTL(KC_RGHT) , KC_MPLY                                                                             , KC_MUTE         , KC_LEFT         , KC_DOWN         , KC_RGHT , KC_INS  , RCTL_T(KC_APP),
 _______            , KC_PAUS  , KC_PGDN       , KC_PSCR   , LCTL(KC_END)  , KC_VOLD     , _______                                             , _______         , RALT(KC_M)      , KC_END          , KC_PSCR         , KC_PGDN , KC_PAUS , _______       ,
-												_______     , _______       , _______     , _______  , LT(_THIRD,KC_DEL)  , LT(_THIRD,KC_ESC)   , _______         , _______         , _______         , _______
+											  _______     , _______       , _______     , _______  , LT(_THIRD,KC_DEL)  , LT(_THIRD,KC_ESC)   , _______         , _______         , _______         , _______
 
 )                  ,
 
