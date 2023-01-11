00e0e9f46c73d32068491faca0f34df5e7b290e2
dtorshin <dtorshin@moysklad.ru>
2023-01-11 10:32:10 +0600

    fix config.h

--- keyboards/dima2/rev2/config.h
+++ keyboards/dima2/rev2/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0000
 #define MANUFACTURER    Dima
 #define PRODUCT         dima2
-#define DESCRIPTION     An ergo keyboard
 
 /* key matrix size */
 // Rows are doubled-up
